67f6ba1d5eb160875a22271cfdf0339e2d1edc7d
gregoryanthony <gregory.anthony@icloud.com>
2026-02-21 13:56:05 -0500

    Update sw.js
    Updated to fix Homescreen app

--- sw.js
+++ sw.js
@@ -1,16 +1,6 @@
-const CACHE_NAME = 'time-tracker-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
-];
+const CACHE_NAME = 'time-tracker-v2';
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
   self.skipWaiting();
 });
 
@@ -25,6 +15,14 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    fetch(e.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(e.request, clone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
